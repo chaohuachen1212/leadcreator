b8495671469f16cdfd440b09bd1d98b7c7de0c29
Hugo Chen <chaohuachen@Hugos-MBP.lan>
2025-12-22 18:38:00 -0800

    added new favicon.

--- wp-content/themes/leadcreator/header.php
+++ wp-content/themes/leadcreator/header.php
@@ -4,7 +4,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.png" />
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon-new.png" />
+<link rel="icon" href="https://chcreator.com/wp-content/uploads/2025/12/favicon-new.png" sizes="any">
 <title><?php wp_title( '-', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
